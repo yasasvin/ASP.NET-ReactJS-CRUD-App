5546b4355e20d19dca955bb801df807d9a722101
yasasvin <yasasvin222@gmail.com>
2019-05-28 06:54:24 +1200

    react working

--- onboardapp/Scripts/react/index.jsx
+++ onboardapp/Scripts/react/index.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
- 
-const app = document.getElementById('main');
-ReactDOM.render(<div>Hello World!</div>, app);
+
+ReactDOM.render(
+    <div>
+        <h1>Hello World!</h1>
+    </div>,
+    document.getElementById('main')
+);
+
